e984ec9c77ce41c5eb7540af08e1caa899ab60d2
karthik <bairojukarthikeya@gmail.com>
2024-01-09 20:20:41 +0530

    String palindrome using reverse() keyword

---
+++ ReverseKeyword.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ReverseKeyword {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        StringBuffer sb = new StringBuffer(str);
+        sb.reverse();
+        if(str.equals(sb.toString()))
+        System.out.print("Palindrome");
+        else
+        System.out.print("Not Palindrome");
+        sc.close();
+    }
+}
