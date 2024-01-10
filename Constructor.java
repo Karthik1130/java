078e24827efa9870db9314ad38e50c7246b26a5b
karthik <bairojukarthikeya@gmail.com>
2024-01-10 21:37:27 +0530

    Constructor implementation

---
+++ Constructor.java
@@ -0,0 +1,13 @@
+public class Constructor {
+    String name;
+    int rno;
+    Constructor(){
+        System.out.print("HI ");
+        name = "MAX";
+        rno = 55;
+    }
+    public static void main(String[] args) {
+        Constructor obj = new Constructor();
+        System.out.print(obj.name+"\n"+obj.rno);
+    }
+}
