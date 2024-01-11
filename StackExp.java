e96fae3d1081a235481168a778e731698e7d12dc
karthik <bairojukarthikeya@gmail.com>
2024-01-11 12:57:17 +0530

    Stack 8d task

---
+++ StackExp.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+public class StackExp {
+    public static void main(String[] args) {
+        Stack <Integer> stk = new Stack<>();
+        boolean result = stk.empty();
+        System.out.println("Is the stack empty? "+result);
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        stk.push(4);
+        System.out.println("Elements in Stack"+stk);
+        result = stk.empty();
+        System.out.println("Is the stack empty? "+result);
+    }
+}
