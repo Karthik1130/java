c423244535c9c5233b87f0f74895931f63a6d872
karthik <bairojukarthikeya@gmail.com>
2024-01-11 11:59:57 +0530

    Checked and Unchecked Exceptions

---
+++ CheckedEx.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class CheckedEx {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4};
+        System.out.println(a[5]);
+    }    
+}
---
+++ UnCheckedEx.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class UnCheckedEx {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4};
+        try{
+            System.out.println(a[4]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Out of index");
+        }
+    }
+}
