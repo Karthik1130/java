abb4a86c360577a497991f14aa0f8c40900448ee
karthik <bairojukarthikeya@gmail.com>
2024-01-13 22:52:47 +0530

    String Palindrome

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -14,11 +14,11 @@ public static boolean isPalindrome(String s){
     int low = 0;
     int high = s.length()-1;
     while(low<high){
-        if(s.charAt(low)!=s.charAt(high))
-        return false;
-        low++;
-        high--;
-    }
-    return true;
+    if(s.charAt(low)!=s.charAt(high))
+    return false;
+    low++;
+    high--;   
+}
+return true;
 }
 }
