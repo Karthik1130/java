01c6e808b45503465a73ae88f8aa8e1834da2e27
karthik <bairojukarthikeya@gmail.com>
2024-01-15 21:15:15 +0530

    asc order names

--- NamesInAscOrder.java
+++ NamesInAscOrder.java
@@ -6,21 +6,26 @@ public class NamesInAscOrder{
         int n = sc.nextInt();
         String[] names = new String[n];
         sc.nextLine();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             System.out.print("Enter name "+(i+1)+":");
             names[i] = sc.nextLine();
         }
-        String temp;
-        for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
-                if(names[j-1].compareTo(names[j])>0){
+            String temp;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(names[j-1].compareTo(names[j])>0)
+                {
                     temp = names[j-1];
                     names[j-1] = names[j];
                     names[j] = temp;
                 }
             }
         }
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             System.out.println("Sorted name "+(i+1)+" :"+names[i]);
         }
         sc.close();
