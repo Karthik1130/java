59d59e5443162fe64eaa73ae8b90ad2ac8bd5eaf
karthik <bairojukarthikeya@gmail.com>
2024-01-15 22:21:15 +0530

    String Palindrome modified

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,24 +1,22 @@
 import java.util.*;
 public class StringPalindrome{
-    public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a String: ");
-    String s = sc.nextLine();
-    if(isPalindrome(s))
-    System.out.println(s+ " is a Palindrome");
-    else
-    System.out.println(s+" is not a Palindrome");
-    sc.close();
-}
-public static boolean isPalindrome(String s){
-    int low = 0;
-    int high = s.length()-1;
-    while(low<high){
-    if(s.charAt(low)!=s.charAt(high))
-    return false;
-    low++;
-    high--;   
-}
-return true;
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        if(isPalindrome(s))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+    public static boolean isPalindrome(String s){
+        int low = 0;
+        int high = s.length()-1;
+        while(low<=high){
+            if(s.charAt(low)!=s.charAt(high))
+                return false;
+            low++;
+            high--;
+        }
+        return true;
+    }
 }
